064ae5619653d44cf27307271e4d2ef6c4bfcffa
Ryan-Otoole <otoolerj@gmail.com>
2022-10-25 10:59:45 -0500

    setup some tooling for start scripts in package.json

--- build/index.js
+++ build/index.js
@@ -1,1 +1,6 @@
-console.log('hi there');
+"use strict";
+console.log(123);
+const test = () => {
+    console.log('this is only a test');
+};
+test();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('hi there')
+console.log(123);
+
+const test = () => {
+    console.log('this is only a test');
+}
+test();
